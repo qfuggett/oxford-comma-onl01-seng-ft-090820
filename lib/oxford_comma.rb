90def8092b27497aed6a5dbf8ed74aea381fc160
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-12 20:31:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,9 @@
 def oxford_comma(array)
-  if array.count == 3
+  if array.count >= 3
     word = "and " + array.last
     array.pop
     array.push(word)
     array.join(", ") 
-  elsif array.count > 3
-    array.join(", ")
   else  
     array.join(",")
     array.join(" and ")
