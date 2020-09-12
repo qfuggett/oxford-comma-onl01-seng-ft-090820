06cb6edbe0384d482c14fd335b938dff4bd1b870
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-12 20:21:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count == 3
+    array.join(", ") 
+    array.push(" and ")
+  elsif array.count > 3
+    array.join(", ")
+  else  
+    array.join(",")
+    array.join(" and ")
+  end
 end
