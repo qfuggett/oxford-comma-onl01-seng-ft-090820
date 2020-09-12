a7d6ad32375625c0036df85b50d77a98247b94d1
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-12 20:34:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count >= 3
+    word = "and " + array.last
+    array.pop
+    array.push(word)
+    array.join(", ") 
+  else  
+    array.join(",")
+    array.join(" and ")
+  end
 end
