badeed66461631189c5bb1f09c92147dfa813bb3
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-12 20:30:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.count == 3
     word = " and " + array.last
+    array.pop
+    array.push(word)
     array.join(", ") 
   elsif array.count > 3
     array.join(", ")
